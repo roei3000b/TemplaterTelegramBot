cefc43b893c53e4b0f2b35b7cddecb51add3b4fd
roei3000b <roei3000b@gmail.com>
2024-02-16 09:48:48 +0200

    Fix parser bug

--- ptb/templater/utils.py
+++ ptb/templater/utils.py
@@ -14,7 +14,7 @@ def round_up_to_nearest_5_minutes(time_str):
     datetime_obj = datetime.strptime(time_str, '%H:%M')
     minutes = datetime_obj.minute
     if minutes % 5 != 0:
-        minutes = minutes + (5 - minutes % 5)
+        minutes = (minutes + (5 - minutes % 5))%60
     new_datetime_obj = datetime_obj.replace(minute=minutes)
     return new_datetime_obj.time().strftime('%H:%M')
 
